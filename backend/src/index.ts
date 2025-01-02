a37e95a701ab3e20824b9a53bf6423d0b89d01e8
miig369 <m.gochera@outlook.com>
2025-01-02 02:25:48 +0000

    search recipes endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,23 @@
 import express from 'express';
 import dotenv from "dotenv";
 import cors from "cors";
+import * as RecipeAPI from "./recipe-api"
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
 
-
 //middleware
 app.use(express.json())
 app.use(cors())
 
-app.get("/", (req, res)=>{
-    res.json({message: "hello world"})
+app.get("/api/recipes/search", async(req, res)=>{
+    // GET http://localhost:8080/api/recipes/search?searchTerm=beef&page=2
+
+    const searchTerm = req.query.searchTerm as string
+    const page = parseInt(req.query.page as string)
+    const results = await RecipeAPI.searchRecipes(searchTerm, page)
+    res.json(results)
 })
 
 app.listen(PORT, ()=> {
---
+++ backend/src/recipe-api.ts
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const apiKey = process.env.API_KEY
+
+export const searchRecipes = async(searchTerm: string, page: number) => {
+
+    if(!apiKey){
+        throw new Error("API key not available")
+    }
+
+    const url = new URL("https://api.spoonacular.com/recipes/complexSearch");
+
+    const queryParams = {
+        apiKey,
+        query: searchTerm,
+        number: "20",
+        offset: (page * 20).toString()
+    }
+    
+    url.search = new URLSearchParams(queryParams).toString();
+
+    try{
+        const response = await fetch(url)
+        const result = await response.json();
+        return result;
+    } catch(error){
+        console.log(error)
+    }
+}
